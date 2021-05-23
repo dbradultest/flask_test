27165a8a398243c39a7817e643d6d218a004f9b4
Dmitriy Bradul <DBradul@gmail.com>
2021-05-23 16:08:52 +0300

    Fetching data from Sqlite DB

---
+++ db.py
@@ -0,0 +1,13 @@
+import os
+import sqlite3
+
+
+def execute_query(query, args=()):
+    db_path = os.path.join(os.getcwd(), 'chinook.db')
+    # db_path = 'chinook.db')
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    cur.execute(query, args)
+    conn.commit()
+    records = cur.fetchall()
+    return records
