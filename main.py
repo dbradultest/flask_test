372e7ec40bf39bcb56f321e6788aad0d7a75fa31
Dmitriy Bradul <DBradul@gmail.com>
2021-05-27 19:41:50 +0300

    Added experimental

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import datetime
-import random
-import string
-
 from flask import Flask, request, Response
 from webargs.flaskparser import use_kwargs
 from webargs import fields, validate
 
-from html_formatters import format_list
+from db import execute_query
+from html_formatters import format_list, format_records
 from utils import generate_password, get_current_time, generate_students
 
 app = Flask(__name__)
@@ -57,5 +54,31 @@ def get_students():
     students = generate_students(10)
     result = format_list(students)
     return result
+
+@app.route("/customers")
+@use_kwargs({
+    "first_name": fields.Str(
+        required=False
+    ),
+    "last_name": fields.String(
+        required=False
+    )},
+    location="query"
+)
+def get_customers(first_name=None, last_name=None):
+    query = f"select * from customers"
+
+    where_filter = {}
+    if first_name:
+        where_filter['FirstName'] = first_name
+    if last_name:
+        where_filter['LastName'] = last_name
+
+    if where_filter:
+        query += ' WHERE ' + ' OR '.join(f'{k}=\'{v}\'' for k, v in where_filter.items())
+
+    records = execute_query(query)
+    # print(records)
+    return format_records(records)
 
 app.run(debug=True, port=5001)
