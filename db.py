685230582540e6bf818da798e590259a6dea6b44
AndriiLatysh <andrii.latysh@gmail.com>
2021-05-27 19:52:40 +0300

    Split logic between modules

--- db.py
+++ db.py
@@ -11,3 +11,19 @@ def execute_query(query, args=()):
     conn.commit()
     records = cur.fetchall()
     return records
+
+def get_customers_by_name(first_name, last_name):
+    query = f"select * from customers"
+
+    where_filter = {}
+    if first_name:
+        where_filter['FirstName'] = first_name
+    if last_name:
+        where_filter['LastName'] = last_name
+
+    if where_filter:
+        query += ' WHERE ' + ' OR '.join(f'{k}=\'{v}\'' for k, v in where_filter.items())
+
+    records = execute_query(query)
+
+    return records
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, Response
 from webargs.flaskparser import use_kwargs
 from webargs import fields, validate
 
-from db import execute_query
+from db import execute_query, get_customers_by_name
 from html_formatters import format_list, format_records
 from utils import generate_password, get_current_time, generate_students
 
@@ -66,18 +66,7 @@ def get_students():
     location="query"
 )
 def get_customers(first_name=None, last_name=None):
-    query = f"select * from customers"
-
-    where_filter = {}
-    if first_name:
-        where_filter['FirstName'] = first_name
-    if last_name:
-        where_filter['LastName'] = last_name
-
-    if where_filter:
-        query += ' WHERE ' + ' OR '.join(f'{k}=\'{v}\'' for k, v in where_filter.items())
-
-    records = execute_query(query)
+    records = get_customers_by_name(first_name=None, last_name=None)
     # print(records)
     return format_records(records)
 
